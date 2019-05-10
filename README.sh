4fe71ba483b1aecb87ce1fb33c31a555b65dcced
Quentin F <1637101+QuentinForand@users.noreply.github.com>
2019-05-10 08:02:13 +0200

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,19 @@
+Displays the current date time like so
+
+```bash
+📅 2019-05-10 🕖 07:55
+```
+
+## Usage
+
+Just add the following block to your `~/.i3/i3blocks.conf`
+
+```
+[time]
+command=<path to this repo>/clock.sh
+interval=60
+```
+
+You can lower the interval as this one may give a 1mn diff
+between real time and display time in the worst case scenario
+_e.g._ it's 8:00 but it display 7:59.
